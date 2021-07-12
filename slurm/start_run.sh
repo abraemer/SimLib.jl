10ddf1a5bcb483fa6f0bd4212e90fccb2facf867
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-07-12 17:33:35 +0200

    Fix start_run.sh

--- slurm/start_run.sh
+++ slurm/start_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! (( $# == 4 ))
 then
-    echo "Usage: start_run.sh N D ALPHA GEOM"
+    echo "Usage: start_run.sh GEOM N D ALPHA"
     exit
 fi
 sbatch --output="logs/run-ed-N_$1-$2d-alpha_$3-slurm-%j.out" slurm/run_ed.jl $1 $2 $3 $4 $(seq -1.5 0.2 1.1) 0.0 $(seq -0.5 0.025 0.3)
